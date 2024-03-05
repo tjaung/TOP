033e09abd201df90d4bb42f3f8d4f96d905e340d
tjaung <88387704+tjaung@users.noreply.github.com>
2024-03-05 16:36:11 -0500

    Add files via upload

---
+++ JS_path/JS/Exercises/npm/webpack-demo/2_asset_management/src/index.js
@@ -0,0 +1,39 @@
+import _ from 'lodash'
+import './style.css'
+import Icon from './icon.jpeg';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    // Lodash imported by script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // Add image to existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+
+    return element;
+}
+
+document.body.appendChild(component());
